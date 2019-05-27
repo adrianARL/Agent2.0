d4620bb19ea37a89845414f36b69a3d54f763925
ilyas <elidrissiachahbar.ilyas@gmail.com>
2019-05-27 17:37:22 +0200

    ilyas - unificar respuestas en una misma funcion

--- SEX/service_execution.py
+++ SEX/service_execution.py
@@ -24,26 +24,15 @@ class ServiceExecution:
         while True:
             if len(self.agent.services) > 0:
                 service = self.agent.services.pop(0)
+                if self.agent.node_info["role"] != "agent":
+                    reg_service = self.agent.topology_manager.get_service(service["service_id"])
+                    self.fill_service(service, reg_service)
                 if self.can_execute_service(service, self.agent.node_info):
                     print("Puedo ejecutar {}".format(service.items()))
                     result = self.agent.runtime.execute_service(service)
-                    if "id" in service.keys():
-                        if self.agent.node_info["role"] != "agent":
-                            id = service["origin_id"]
-                            agent_id = self.service_ids[id]["agent_id"]
-                            result["id"] = self.service_ids[id]["origin_id"]
-                            self.agent.send_dict_to(result, agent_id)
-                        else:
-                            self.agent.send_dict(result)
-                elif(self.agent.node_info['role'] != "agent"):
+                    self.agent.services_results.append(result)
+                elif(self.agent.node_info["role"] != "agent"):
                     # print("Delego el servicio a un agent {}".format(service.items()))
-                    random_id = self.generate_id()
-                    service["origin_id"] = service["id"]
-                    service["id"] = random_id
-                    self.service_ids[random_id] = {
-                        "origin_id": service['origin_id'],
-                        "agent_id": service['agent_id']
-                    }
                     th_attend_service = Thread(target=self.attend_service, args=(service, ))
                     th_attend_service.start()
                     self.th_attend_services.append(th_attend_service)
@@ -59,6 +48,7 @@ class ServiceExecution:
         if "service_id" in service.keys():
             reg_service = self.agent.topology_manager.get_service(service["service_id"])
             agents = self.agent.topology_manager.get_my_agents(self.agent.node_info["zone"])
+            attended = False
             if(agents):
                 for agent in agents:
                     print("Agent : ", agent.get("IoT"))
@@ -67,7 +57,17 @@ class ServiceExecution:
                         reg_service["id"] = service["id"]
                         reg_service["type"] = service["type"]
                         self.agent.send_dict_to(reg_service, agent["nodeID"])
+                        attended = True
                         break
+            if not attended:
+                result = {
+                    "type": "service_result",
+                    "id": service["id"],
+                    "status": "unattended",
+                    "output": ""
+                }
+                self.agent.services_results.append(result)
+
 
 
 
@@ -85,4 +85,23 @@ class ServiceExecution:
                     id = service_result["id"]
                     agent_id = self.service_ids[id]["agent_id"]
                     service_result["id"] = self.service_ids[id]["origin_id"]
+                    print("Respondo: ", service_result)
                     self.agent.send_dict_to(service_result, agent_id)
+                else:
+                    self.agent.send_dict(result)
+
+
+    def fill_service(self, service, reg_service):
+        random_id = self.generate_id()
+        service["origin_id"] = service["id"]
+        service["id"] = random_id
+        self.service_ids[random_id] = {
+            "origin_id": service["origin_id"],
+            "agent_id": service["agent_id"]
+        }
+        print("fill service:")
+        print(self.service_ids)
+        print()
+        for key in reg_service.keys():
+            if not key in service.keys():
+                service[key] = reg_service[key]
