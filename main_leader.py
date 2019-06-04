d5e07ecaff3a7458c3250cf7b4d982eb44abdd99
ilyas <elidrissiachahbar.ilyas@gmail.com>
2019-06-04 20:20:28 +0200

    dsf

--- main_leader.py
+++ main_leader.py
@@ -4,14 +4,14 @@ import subprocess
 
 node_id = {
 	# "ipDB" : "192.168.1.34",
-	# "ipDB" : "10.0.2.16",
-	"ipDB" : "127.0.0.1",
+	"ipDB" : "10.0.2.16",
+	# "ipDB" : "127.0.0.1",
 	"portDB" : 8000,
 	"device" : "Leader A",
 	"role" : "leader",
 	"myIP" : subprocess.getoutput("hostname -I | awk '{print $1}'"),
-	# "leaderIP" : '10.10.176.123',
-	"leaderIP" : '192.168.1.134',
+	"leaderIP" : '10.10.176.123',
+	# "leaderIP" : '192.168.1.134',
 	"port" : 5000,
 	"IoT" : ["-", "semaforo"],
 	"broadcastIP" : subprocess.getoutput("ip a | grep inet | grep brd | awk '{print $4}'"),
