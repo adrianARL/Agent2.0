4f15f48bdd1f50213658d640bddb261437442751
arivas <arivas@ako.com>
2019-06-08 11:50:57 +0200

    mainagent

--- main_agent.py
+++ main_agent.py
@@ -4,16 +4,16 @@ import time
 from agent import Agent
 
 node_id = {
-	"ipDB" : "10.0.2.16",
-	# "ipDB" : "192.168.1.34",
+	# "ipDB" : "10.0.2.16",
+	"ipDB" : "192.168.1.45",
 	"portDB" : 8000,
 	"device" : "Agent",
 	"role" : "agent",
 	"myIP" : subprocess.getoutput("hostname -I | awk '{print $1}'"),
-	# "leaderIP" : '192.168.1.34',
-	"leaderIP" : '10.0.2.16',
+	"leaderIP" : '192.168.1.45',
+	# "leaderIP" : '10.0.2.16',
 	"port" : 5000,
-	"IoT" : ["-", "-"], # necesario > 1 IoT para que api no lo transforme a string en lugar de lista
+	"IoT" : ["-", "rfid"], # necesario > 1 IoT para que api no lo transforme a string en lugar de lista
 	"broadcastIP" : subprocess.getoutput("ip a | grep inet | grep brd | awk '{print $4}'"),
 	"cpu" : psutil.cpu_percent(),
 	"ram" : psutil.virtual_memory()[2],
