1e3e3eaf564f29926a4531002656c62c71239cab
arivas <arivas@ako.com>
2019-06-10 17:12:26 +0200

    dd

--- main_virtual.py
+++ main_virtual.py
@@ -4,16 +4,16 @@ import time
 from agent import Agent
 
 node_id = {
-	# "ipDB" : "10.0.2.16",
-	"ipDB" : "192.168.1.45",
+	"ipDB" : "10.0.2.16",
+	# "ipDB" : "192.168.1.45",
 	"portDB" : 8000,
 	"device" : "Agent",
 	"role" : "agent",
 	"myIP" : subprocess.getoutput("hostname -I | awk '{print $1}'"),
-	"leaderIP" : '192.168.1.45',
-	# "leaderIP" : '10.0.2.16',
+	# "leaderIP" : '192.168.1.45',
+	"leaderIP" : '10.0.2.16',
 	"port" : 5000,
-	"IoT" : ["-", "motor", "rfid_sensor", "ultrasonic_sensor", "line_sensor", "virtual"], 
+	"IoT" : ["-", "motor", "rfid_sensor", "ultrasonic_sensor", "line_sensor", "virtual"],
 	"broadcastIP" : subprocess.getoutput("ip a | grep inet | grep brd | awk '{print $4}'"),
 	"cpu" : psutil.cpu_percent(),
 	"ram" : psutil.virtual_memory()[2],
