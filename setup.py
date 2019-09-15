99fe61ddf3db19ea3c692d36b496f6682fcdb63a
ilyas <elidrissiachahbar.ilyas@gmail.com>
2019-09-15 22:14:33 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Agent2.0',
+    version='',
+    packages=['RT', 'SEX', 'TRM'],
+    url='',
+    license='',
+    author='ilyas',
+    author_email='',
+    description=''
+)
