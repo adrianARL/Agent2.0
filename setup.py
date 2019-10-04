03313422f771c66abcdf88f034e7cc176d81b1d7
Adrián <adrian96uni@gmail.com>
2019-10-04 19:57:05 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='Agent2.0',
     version='',
-    packages=['RT', 'SEX', 'TRM', 'API', 'Agent', 'Leader'],
+    packages=['agent'],
     url='',
     license='',
     author='ilyas&adrian',
